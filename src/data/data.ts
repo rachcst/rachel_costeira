c0ff0397c0f482e9fc189afbf7303aa0e5bfcd17
rachcst <rachelcosteira@gmail.com>
2026-04-25 18:25:05 +0100

    fixed sidebar and @media in css

--- src/data/data.ts
+++ src/data/data.ts
@@ -3,24 +3,6 @@ export const websiteData = {
   author: "Rachel Costeira"
 }
 
-export const websiteText = {
-  aboutMe: "My name is Rachel. I'm a 21 year old Christian culinary student, but I also do a bit of programming, writing, photographyand a handful of other things. I wanted to create a spacefor myself where I could post about my interests. An archive of sorts. So Rachel's Archive was born."
-}
-
-export const statusData = [
-  {
-    name: "doing",
-    status: "studying for culinary finals"
-  },
-  {
-    name: "listening to",
-    status: "anything by Djo",
-  },
-  {
-    name: "eating",
-    status: "Katsu Curry",
-  }
-]
 
 export const navLinks = [
     { 
@@ -41,101 +23,15 @@ export const navLinks = [
     }
 ]
 
-export const recs = {
-  websites: [
+export const socials = [
     { 
-      name: "PetraPixel", 
-      url: "https://petrapixel.neocities.org/" 
-    },
-    { 
-      name: "Shank Space", 
-      url: "https://knhash.in/" 
-    },
-    { 
-      name: "Alexa Fazio", 
-      url: "https://alexafazio.dev/" 
-    },
-    { 
-      name: "Ita.toys", 
-      url: "https://ita.toys/" 
+      name: "Pinterest", 
+      url: "https://uk.pinterest.com/rachcst1/" 
     },
     {
-      name: "Chen Hui Jing",
-      url: "https://chenhuijing.com/"
-    }
-  ],
-  youtube: [
-    { 
-      name: "TextToSpeech", 
-      url: "#" 
-    }
-  ]
-};
-
-export const buttons = [
-  {
-    url: "https://adriansblinkiecollection.neocities.org/buttons/a11.gif",
-  },
-  {
-    url: "https://adriansblinkiecollection.neocities.org/buttons/a1.gif",
-  },
-  {
-    url: "https://adriansblinkiecollection.neocities.org/buttons/a8.png",
-  },
-  {
-    url: "https://adriansblinkiecollection.neocities.org/buttons/a22.gif",
-  },
-   {
-    url: "https://adriansblinkiecollection.neocities.org/buttons/a60.gif",
-  },
-   {
-    url: "https://adriansblinkiecollection.neocities.org/buttons/a56.png",
-  },
-   {
-    url: "https://adriansblinkiecollection.neocities.org/buttons/a57.png",
-  },
-   {
-    url: "https://pixelsafari.neocities.org/buttons/2stardew.gif",
-  },
-     {
-    url: "https://pixelsafari.neocities.org/buttons/2minecraft.png",
-  },
-  {
-    url: "https://pixelsafari.neocities.org/buttons/1notepad8.gif",
-  },
-  {
-    url: "https://pixelsafari.neocities.org/buttons/1html2.gif",
-  },
-
-    {
-    url: "https://88by31.neocities.org/web/vscbutton.gif",
-  },
-];
-
-export const badges =[
-  {
-    url: "https://pixelsafari.neocities.org/misc/badge/flagbrasil.gif",
-  },
-  {
-    url: "https://pixelsafari.neocities.org/misc/badge/flagportugal.gif",
-  },
-  {
-    url: "",
-  },
-  {
-    url: "",
-  },
-  {
-    url: "",
-  },
-  {
-    url: "",
-  },
-  {
-    url: "",
-  },
-  {
-    url: "",
-  },
+      name: "Github", 
+      url: "https://github.com/rachcst" 
+    },
 
 ]
+
